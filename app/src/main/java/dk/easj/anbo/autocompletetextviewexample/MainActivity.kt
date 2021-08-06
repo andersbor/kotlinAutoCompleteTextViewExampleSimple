24935b7380f19c59aea1cd1c3b73d79c663b2b6d
Anders Børjesson <anbo@zealand.dk>
2021-08-06 12:31:12 +0200

    Initial commit

---
+++ app/src/main/java/dk/easj/anbo/autocompletetextviewexample/MainActivity.kt
@@ -0,0 +1,48 @@
+package dk.easj.anbo.autocompletetextviewexample
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.ArrayAdapter
+import dk.easj.anbo.autocompletetextviewexample.databinding.ActivityMainBinding
+
+/**
+ * As a class AutoCompleteTextView extends EditText (despite the name TextView)
+ * AutoCompleteTextView combines EditText + dropdown list:
+ *     you can choose from the list of write anything you want
+ */
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
+    private val fishNames = arrayOf(
+        "bream", "carp", "grayling", "perch", "pike", "pike-perch", "roach",
+        "tench", "eel", "sturgeon", "salmon", "trout", "smelt", "rainbow trout", "whitefish",
+        "picked dogfish", "shark", "skate", "smooth hound", "anchovy", "herring", "pilchard",
+        "sardine", "sardinella", "sprat", "blue ling", "blue whitling", "cod", "greater forkbeard",
+        "haddock", "hake", "ling", "pollack", "poor cod",
+        "pout", "saithe", "tush", "whiting", "atherine", "bogue", "mullet", "picaral", "scad",
+        "sea bream", "surmullet", "chub mackerel", "garfish",
+        "mackerel", "swordfish", "albacore tuna", "bonito tuna", "skipjack tuna", "tuna"
+    )
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view: View = binding.root
+        setContentView(view)
+
+        fishNames.sort()
+        val adapter = ArrayAdapter(this, android.R.layout.simple_dropdown_item_1line, fishNames)
+
+        binding.fishAutoCompleteTextView.setAdapter(adapter)
+
+        binding.showButton.setOnClickListener {
+            val fish = binding.fishAutoCompleteTextView.text.trim()
+            if (fish.isEmpty()) {
+                binding.fishAutoCompleteTextView.error = "No fish!"
+                return@setOnClickListener
+            }
+            binding.messageTextView.text = "You caught $fish"
+        }
+    }
+}
